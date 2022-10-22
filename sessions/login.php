2c05cbf1d0c0b4ceaeffad8c46c426453c9ae60c
Enrique Saracho Felix <esarachof97@gmail.com>
2022-10-22 13:11:34 -0600

    Create sessions/login.php

---
+++ sessions/login.php
@@ -0,0 +1,23 @@
+<html>
+    <head></head>
+    <body style="font-family: sans-serif;">
+        <h1>Please Log In</h1>
+        <?php
+            if(isset($_SESSION["error"])) {
+                echo('<p style="color: red;">'.$_SESSION["error"]."</p>\n");
+                unset($_SESSION["error"]);
+            }
+            if(isset($_SESSION["success"])) {
+                echo('<p style="color: green;">'.$_SESSION["success"]."</p>\n");
+                unset($_SESSION["success"]);
+            }
+        ?>
+        <form method="post">
+            <p>Account: <input type="text" name="account" value=""/></p>
+            <p>Password: <input type="text" name="pw" value=""/></p>
+            <!-- password is umsi -->
+            <p><input type="submit" value="Log In"/>
+            <a href="app.php">Cancel</a></p>
+        </form>
+    </body>
+</html>
