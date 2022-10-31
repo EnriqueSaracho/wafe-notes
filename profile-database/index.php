4fb7984cda85e4fb94050305e9a553193e252554
Enrique Saracho Felix <esarachof97@gmail.com>
2022-10-31 10:23:24 -0700

    Create profile-database/index.php

---
+++ profile-database/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Enrique Saracho</title>
+    </head>
+    <body>
+        <h1>Enrique Saracho's Resume Registry</h1>
+        <a href="login.php">Please log in</a>
+        <table border="1">
+            <tr><th>Name</th><th>Headline</th></tr>
+        </table>
+    </body>
+</html>
