5a79fa67edbc7d04708b25b88b5f3a0598e718ce
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-01 20:29:35 -0700

    Update profile-database/login.php

--- profile-database/index.php
+++ profile-database/index.php
@@ -1,12 +1,29 @@
+<?php
+session_start();
+?>
+
 <html>
-    <head>
-        <title>Enrique Saracho</title>
-    </head>
-    <body>
-        <h1>Enrique Saracho's Resume Registry</h1>
-        <a href="login.php">Please log in</a>
-        <table border="1">
+
+<head>
+    <title>Enrique Saracho</title>
+</head>
+
+<body>
+    <h1>Enrique Saracho's Resume Registry</h1>
+    <?php
+    if (isset($_SESSION['success'])) {
+        echo ('<p style="color: green">' . $_SESSION['success'] . '</p>');
+        unset($_SESSION['success']);
+    }
+    if (isset($_SESSION['error'])) {
+        echo ('<p style="color: red">' . $_SESSION['error'] . '</p>');
+        unset($_SESSION['error']);
+    }
+    ?>
+    <a href="login.php">Please log in</a>
+    <!-- <table border="1">
             <tr><th>Name</th><th>Headline</th></tr>
-        </table>
-    </body>
+        </table> -->
+</body>
+
 </html>
