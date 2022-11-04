c860d1a3849a40e65dc1caed498e52adaf5546a0
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-03 19:09:19 -0700

    Update profile-database/index.php
    Fixed if statement for determining if user is logged in or not

--- profile-database/index.php
+++ profile-database/index.php
@@ -19,7 +19,7 @@ session_start();
         echo ('<p style="color: red">' . $_SESSION['error'] . '</p>');
         unset($_SESSION['error']);
     }
-    if (!isset($_SESSION['account'])) { ?>
+    if (!isset($_SESSION['user_id'])) { ?>
         <a href="login.php">Please log in</a>
     <?php } else { ?>
         <a href="logout.php">Logout</a><br>
