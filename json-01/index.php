36ef4b325a86a0773b8ddd11b8a679342db612ff
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-09 19:12:40 -0700

    json-01/index.php
    Created file

---
+++ json-01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(data) {
+            $.getJSON('json.php', function(data) {
+                $("#back").html(data.first);
+                window.console && console.log(data);
+            })
+        })
+    </script>
+</head>
+
+<body>
+    <p>Howdy - Lets get some JSON</p>
+    <p id="back">Before</p>
+    <p>
+        <a href="syntax.php" target="_new">JSON Syntax</a> | 
+        <a href="json.php" target="_new">json.php</a>
+    </p>
+</body>
+
+</html>
