607d0f7432b1250b5b0755550befeece9086677f
Enrique Saracho Felix <esarachof97@gmail.com>
2022-11-09 18:33:36 -0700

    json-01/syntax.php
    Updated file

--- json-01/syntax.php
+++ json-01/syntax.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <script type="text/javascript">
+        who = {
+            'name': 'Kiki',
+            'age': 25,
+            'college': true,
+            'offices': ['3350DMC', '3437NQ'],
+            'skills': { 'fortran': 10, 'C': 10, 'C++': 5, 'python': '7'}
+        };
+        window.console && console.log(who);
+    </script>
+    <p>Check out the console.log to see the cool object</p>
 </body>
 </html>
